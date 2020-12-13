9f075299f9582547074bb49d3efb1694573f8817
unknown <adelsonsantospinheiro@gmail.com>
2020-12-13 01:06:24 -0300

    primeira versão

--- src/main/java/com/api/marvel/consumoApiMarvel/ConsumoApiMarvelApplication.java
+++ src/main/java/com/api/marvel/consumoApiMarvel/ConsumoApiMarvelApplication.java
@@ -1,9 +1,9 @@
 package com.api.marvel.consumoApiMarvel;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
