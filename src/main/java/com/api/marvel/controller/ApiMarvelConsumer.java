eb476775c2838a40d32482afad57ae115d19f5af
unknown <adelsonsantospinheiro@gmail.com>
2020-12-13 01:14:03 -0300

    primeiro commit

--- src/main/java/com/api/marvel/controller/ApiMarvelConsumer.java
+++ src/main/java/com/api/marvel/controller/ApiMarvelConsumer.java
@@ -2,7 +2,7 @@ package com.api.marvel.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
